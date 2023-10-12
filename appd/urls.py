ad84196e1c5db75cf3391d83128f5ab4bfecfa4f
hyperman0011 <hyperman0011@gmail.com>
2023-10-12 19:58:13 +0500

    kqnejd

--- appd/urls.py
+++ appd/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from .views import TeacherAPIView, TeacherDetailAPIView, StudentListCreateAPIView, StudentRetrieveAPIView
 
 urlpatterns = [
-    path('teachers/', TeacherAPIView.as_view(), name="teachers"),
-    path('teachers/<int:pk>/', TeacherDetailAPIView.as_view(), name="teachers_detail"),
+    path('teach/', TeacherAPIView.as_view(), name="teachers"),
+    path('teach/<int:pk>/', TeacherDetailAPIView.as_view(), name="teachers_detail"),
     path('students/', StudentListCreateAPIView.as_view(), name="student"),
     path('students/<int:pk>/', StudentRetrieveAPIView.as_view(), name="student")
 ]
